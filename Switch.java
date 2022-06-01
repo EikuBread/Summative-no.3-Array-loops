537d5294c5146cfd3c76821704d2ed8f162f9855
EikuBread <106257475+EikuBread@users.noreply.github.com>
2022-06-01 10:35:27 -0400

    deleted files
    deleted files

--- Switch.java
+++ Switch.java
@@ -8,12 +8,20 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Switch extends Actor
 {
+    boolean black = false;
     /**
      * Act - do whatever the Switch wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
+        if (black ==false)
+        {
+            setImage("Switch/Greenswitch.png");  
+        }
+        if (black ==true)
+        {
+            setImage("Switch/Redswitch.png");  
+        }
     }    
 }
